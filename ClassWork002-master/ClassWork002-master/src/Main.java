a03038b28ae4da0f5c292f7fea7f16364c25275d
gincho georgiev <otivam_92@abv.bg>
2018-05-31 17:35:52 +0300

    Add files via upload

---
+++ ClassWork002-master/ClassWork002-master/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+	
+Scanner scan = new Scanner(System.in);
+
+	System.out.println("String 1: ");
+	String a = scan.nextLine();
+	System.out.println("String 2: ");
+	String b = scan.nextLine();
+	
+	System.out.println("Result: " + a + b);
+
+}
+}
---
+++ ClassWork002-master/ClassWork002-master/src/accumulationandmultiplication.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class accumulationandmultiplication {
+public static void main(String[] args) {
+		
+Scanner scan = new Scanner(System.in);
+
+	System.out.println("First number: ");
+	int a = scan.nextInt();
+	System.out.println("Second number: ");
+	int b = scan.nextInt();
+	System.out.println("Third number: ");
+	int c = scan.nextInt();
+	
+	System.out.println("Result: " + (a+b) * c);
+	
+	
+}
+}
